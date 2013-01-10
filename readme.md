a978d864878db12c492e9a722393cf38b79b4485
acrylian <wartgart@gmx.net>
2013-01-10 19:38:30 +0100

    Some extension of the plugin for more services. Untested yet!

--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
 A Zenphoto plugin for privacy friendly social sharing
 ===============================================================
-Put the file in your /plugins folder and modify your theme to use it. Please see the file comments itself for more usage information
+A plugin that provides a function to add privacy friendly, non tracking social network share buttons (Facebook,Twitter, Google+ and others). 
+
+Put the file in your /plugins folder and modify your theme to use it. Please see the file comments itself for more usage information.
+
+This is an adaption as a Zenphoto plugin of:
+jquery.socialshareprivacy.js | 2 Klicks fuer mehr Datenschutz
+ 
+http://www.heise.de/extras/socialshareprivacy/
+http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html
+ 
+Copyright (c) 2011 Hilko Holweg, Sebastian Hilbig, Nicolas Heiringhoff, Juergen Schmidt,
+Heise Zeitschriften Verlag GmbH & Co. KG, http://www.heise.de
+ 
+and extended by:
+Copyright (c) 2012 Mathias Panzenböck
+https://github.com/panzi/SocialSharePrivacy
+
+MIT License (http://www.opensource.org/licenses/mit-license.php)
--- socialshareprivacy.php
+++ socialshareprivacy.php
@@ -1,17 +1,28 @@
 <?php
 /**
- * This is an adaption of the tool provided by the German Heise Verlag (publisher): http://www.heise.de/extras/socialshareprivacy/
+ * A basic plugin that provides a function to add privacy friendly, non tracking social network share buttons 
+ * (Facebook,Twitter, Google+ and more). This is an adaption as a Zenphoto plugin of:
+ * jquery.socialshareprivacy.js | 2 Klicks fuer mehr Datenschutz
  *
- * A basic plugin that provides a function to add privacy friendly social network share buttons (Facebook,Twitter, Google+). 
- * Since these buttons are under suspicion that they transfer data even from normal visitors even if they are not logged in
- * to any of these services this plugin features switches to turn them explicitly on.
+ * http://www.heise.de/extras/socialshareprivacy/
+ * http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html
  *
- * Only one button call per page allowed. So use best for single items or generally at the bottom or in the sidebar of your site.
- * You might need to adjust/override the plugin's default css stylings. Place <?php printSocialSharePrivacyButtons(); ?> for the buttons
+ * Copyright (c) 2011 Hilko Holweg, Sebastian Hilbig, Nicolas Heiringhoff, Juergen Schmidt,
+ * Heise Zeitschriften Verlag GmbH & Co. KG, http://www.heise.de
  *
- * The plugin does not provide any options. Modify the plugin itself in the jQuery calls to adjust to your liking.
+ * and extended by:
+ * Copyright (c) 2012 Mathias Panzenböck
+ * https://github.com/panzi/SocialSharePrivacy
  *
- * Note: The plugin is in German only currently. Localizsation probably follows later.
+ * A basic plugin that provides a function to add privacy friendly, non tracking social network share buttons (Facebook,Twitter, Google+ and others). 
+ *
+ * Place <?php printSocialSharePrivacyButtons(); ?> where you wish the buttons to appear.
+ * You might need to adjust/override the plugin's default css stylings to fit your site design. 
+ *
+ * The plugin does not provide any options for the sharing itself. Modify the jQuery calls within printSocialSharePrivacyButtons() to your liking.
+ *
+ * Note: The button text are available in English, French and German. The plugin itself is not yet translated.
+ *
  *
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  * @author Malte Müller (acrylian)
@@ -19,64 +30,128 @@
  * @package plugins
  */
 
-$plugin_description = gettext_pl('A basic plugin that provides a function to add privacy friendly social network share buttons (Facebook,Twitter, Google+). A visitor has to be enable them explicitly first so they do not transfer unwanted data. An adaption of http://www.heise.de/extras/socialshareprivacy/',"socialshareprivacy");
+$plugin_description = gettext_pl('A basic plugin that provides a function to add privacy friendly, non tracking social network share buttons (Facebook,Twitter, Google+ and more). An adaption as Zenphoto plugin of http://www.heise.de/extras/socialshareprivacy/ and https://github.com/panzi/SocialSharePrivacy - MIT License (http://www.opensource.org/licenses/mit-license.php).',"socialshareprivacy");
 $plugin_author = 'Malte Müller (acrylian)';
-$plugin_version = '1.4.3';
-zp_register_filter('theme_head','socialprivacyshareJS');
+$plugin_version = '1.4.4';
+$option_interface = 'socialshareprivacy_options';
+zp_register_filter('theme_head','socialshareprivacyJS');
 
+class demoplugin_options {
+
+	/**
+	 * class instantiation function
+	 */
+	function __construct() {
+	
+	}
+	
+	function getOptionsSupported() {
+		/* 
+			The option definitions are stored in a multidimensional array. There are several predefine option types.
+			Options types are the same for plugins and themes.
+		*/
+		$options = array(
+			gettext_pl('Social networks','socialshareprivacy') => array(
+				'key' => 'socialshareprivacy_socialnetworks',
+				'type' => OPTION_TYPE_CHECKBOX_UL,
+				'order' => 0,
+				'checkboxes' => array( // The definition of the checkboxes
+					gettext_pl('Facebook','socialshareprivacy')=>'socialshareprivacy_facebook',
+					gettext_pl('Twitter','socialshareprivacy')=>'socialshareprivacy_twitter',
+					gettext_pl('Google+','socialshareprivacy')=>'socialshareprivacy_googleplus',
+					gettext_pl('Tumblr','socialshareprivacy')=>'socialshareprivacy_tumblr',
+					gettext_pl('Reddit','socialshareprivacy')=>'socialshareprivacy_Reddit',
+					gettext_pl('Pinterest','socialshareprivacy')=>'socialshareprivacy_pinterest',
+					gettext_pl('flattr','socialshareprivacy')=>'socialshareprivacy_flattr',
+					gettext_pl('Stumbleupon','socialshareprivacy')=>'socialshareprivacy_stumbleupon',
+					gettext_pl('Mail','socialshareprivacy')=>'socialshareprivacy_mail',
+					gettext_pl('linkedin','socialshareprivacy')=>'socialshareprivacy_linkedin',
+					gettext_pl('xing','socialshareprivacy')=>'socialshareprivacy_xing',
+					gettext_pl('buffer','socialshareprivacy')=>'socialshareprivacy_buffer',
+					gettext_pl('delicious','socialshareprivacy')=>'socialshareprivacy_delicious',
+					gettext_pl('disqus','socialshareprivacy')=>'socialshareprivacy_disqus',
+				),
+				'desc' => gettext_pl('Select the social networks you wish buttons to appear','socialshareprivacy'))
+		);
+	}
+}
 /**
 * Adds the jQuery calls to the theme head via filter
 */
-function socialprivacyshareJS() {
+function socialshareprivacyJS() {
 	$locale = getUserLocale();
-	$txt_info = gettext_pl('2 clicks for more privacy: The button to share is enabled only if you click here first. On enabling data is submitted to third parties - see <em>i</em>.',"socialshareprivacy");
 	?>
-	<script type="text/javascript" src="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/socialshareprivacy/jquery.socialshareprivacy.js"></script>
-	<script type="text/javascript">
-    jQuery(document).ready(function($){
-      if($('#socialshareprivacy').length > 0){
-        $('#socialshareprivacy').socialSharePrivacy({
-        	txt_help : '<?php echo gettext_pl("If you enable these fields by clicking, data will be submitted to Facebook, Twitter or Google in the USA and possibly stored there. For more information click the <em>i</em> (German only).","socialshareprivacy"); ?>',
-         	css_path: '<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/socialshareprivacy/socialshareprivacy/socialshareprivacy.css',
-         	settings_perma: '<?php echo gettext_pl("Activate persistent and agree to data submission:","socialshareprivacy"); ?>',
-         	services : {
-   					facebook : {
-   						'status' : 'on',
-      				'perma_option': 'on',
-      				'dummy_img': '<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/socialshareprivacy/socialshareprivacy/images/dummy_facebook_en.png',
-      				'txt_info': '<?php echo $txt_info; ?>',
-      				'language': '<?php echo $locale; ?>'
-    				}, 
-    				twitter : {
-        			'status' : 'on',
-        			'perma_option': 'on',
-        			'dummy_img': '<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/socialshareprivacy/socialshareprivacy/images/dummy_twitter.png',
-        			'txt_info': '<?php echo $txt_info; ?>',
-        			'language': '<?php echo $locale; ?>'
-    				},
-    				gplus : {
-    					'status' : 'on',
-    					'perma_option': 'on',
-      				'display_name' : 'Google+',
-      				'dummy_img': '<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/socialshareprivacy/socialshareprivacy/images/dummy_gplus.png',
-      				'txt_info': '<?php echo $txt_info; ?>',
-      				'language': '<?php echo $locale; ?>'
-    				}
-  				}
-        }); 
-      }
-    });
-  </script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/jQuery-Cookies/raw/master/jquery.cookies.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.facebook.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.twitter.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.gplus.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.tumblr.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.reddit.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.pinterest.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.flattr.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.stumbleupon.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.mail.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.linkedin.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.xing.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.buffer.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.delicious.js"></script>
+	<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts/jquery.socialshareprivacy.disqus.js"></script>
 	<?php
+	$dir = '';
+	
+	//Get the sub dir for the supported button languages.
+	switch($locale) {
+  	case 'de_DE':
+  		$dir = '/de';
+  		break;
+  	case 'fr_FR':
+  		$dir = '/fr';
+  		break;
+  }
+  switch($locale) {
+  	case 'de_DE':
+  	case 'fr_FR':
+ 		 ?>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.facebook.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.twitter.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.gplus.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.tumblr.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.reddit.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.pinterest.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.flattr.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.stumbleupon.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.mail.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.linkedin.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.xing.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.buffer.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.delicious.js"></script>
+			<script type="text/javascript" src="<?php echo SERVERPATH.'/'.USER_PLUGIN_FOLDER.'; ?>/socialshareprivacy/scripts<?php echo $dir; ?>/jquery.socialshareprivacy.disqus.js"></script>
+  		<?php
+  		break;
+  	}
 }
 
 /**
-* Place this where you wish the buttons to appear. Use only once per page otherwise you get CSS conflicts! 
-* For multiple usages per page you need to do it manually.
+* Place this where you wish the buttons to appear. The plugin includes also jQUery calls to set the buttons up to allow multiple button sets per page.
+*  
+* @param string $class The CSS class to assign to the element. Default 'socialshareprivacy'. For multiple button sets per page set individual classes.
 */
-function printSocialSharePrivacyButtons() {
+function printSocialSharePrivacyButtons($class='socialshareprivacy') {
 	?>
-	<div id="socialshareprivacy"></div>
+	<script type="text/javascript">
+		// <![CDATA[
+		// Default order of buttons:
+		$.fn.socialSharePrivacy.settings.order = ['facebook', 'gplus', 'twitter', 'xing'];
+		//$.fn.socialSharePrivacy.settings.path_prefix = '../';
+
+		$(document).ready(function () {
+			$('.<?php echo $class; ?>').socialSharePrivacy();
+		});
+	// ]]>
+	</script>
+	<div class="socialshareprivacy"></div>
 	<?php
 }
 ?>
